9a7635c431376e5eccb41b941fd32918985b55bf
eclipseagency <mustafa.halawa9@gmail.com>
2026-04-27 00:17:03 +0300

    fix: fix import path in SignUp.tsx

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import api from '../../services/api';
+import api from '../services/api';
 
 const SignUp = () => {
   const navigate = useNavigate();
